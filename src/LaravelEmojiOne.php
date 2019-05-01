fb21d2e2f9b9096b4f97aec77b83760da5d14596
Amrani Houssain <amranidev@gmail.com>
2019-05-01 23:29:02 +0100

    adopt psr-2

--- src/LaravelEmojiOne.php
+++ src/LaravelEmojiOne.php
@@ -7,8 +7,14 @@ use Emojione\Ruleset;
 
 class LaravelEmojiOne
 {
+    /**
+     * @var \Emojione\Client
+     */
     private $client;
 
+    /**
+     * LaravelEmojiOne constructor.
+     */
     public function __construct()
     {
         $this->client = new Client(new Ruleset());
@@ -20,37 +26,58 @@ class LaravelEmojiOne
 
         if (config('emojione.imagePathPNG')) {
             $this->client->imagePathPNG = url(config('emojione.imagePathPNG')) . '/';
-        }
-        else {
+        } else {
             // Use the CDN if 'imagePathPNG' config is not set
             $this->client->imagePathPNG = 'https://cdn.jsdelivr.net/emojione/assets' . '/' . $this->client->emojiVersion . '/png/' . $this->client->emojiSize . '/';
         }
-        
-        // config ascii option added ternary incase option isn't part of config 
+
+        // config ascii option added ternary in case option isn't part of config
         $this->client->ascii = config('emojione.ascii') ? true : false;
-        
     }
 
+    /**
+     * @param $str
+     *
+     * @return string
+     */
     public function toImage($str)
     {
         return $this->client->toImage($str);
     }
 
+    /**
+     * @param $str
+     *
+     * @return string
+     */
     public function toShort($str)
     {
         return $this->client->toShort($str);
     }
 
+    /**
+     * @param $str
+     *
+     * @return string
+     */
     public function shortnameToImage($str)
     {
         return $this->client->shortnameToImage($str);
     }
 
+    /**
+     * @param $str
+     *
+     * @return string
+     */
     public function unicodeToImage($str)
     {
         return $this->client->unicodeToImage($str);
     }
 
+    /**
+     * @return \Emojione\Client
+     */
     public function getClient()
     {
         return $this->client;
